f9f9572c4d7814c5a23eb2756dd13a4f665bd07b
Artem Belsky <belsartem@gmail.com>
2024-03-17 17:52:27 +0100

    Выполненное задание к уроку 12

---
+++ src/test/java/qa/abels/TestBase.java
@@ -0,0 +1,27 @@
+package qa.abels;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        open("https://github.com");
+    }
+
+    @AfterEach()
+    void afterEach() {
+        Selenide.closeWebDriver();
+    }
+}
